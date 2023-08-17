37eb8976316a90ee758ae00a2fc7938a131f5019
johngerving <gervingjohn@gmail.com>
2023-08-16 22:43:31 -0700

    Added to form, changed styling

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, dialog, ipcMain } = require("electron");
 const path = require("path");
 const unhandled = require("electron-unhandled");
 
+// Electron error handling
 unhandled({
   logger: () => {
     console.error();
@@ -9,25 +10,27 @@ unhandled({
   showDialog: true,
 });
 
+// Open file explorer to allow user to select files
 async function handleFileOpen() {
   const { canceled, filePaths } = await dialog.showOpenDialog({
-    properties: ["openFile", "multiSelections"],
+    properties: ["openFile", "multiSelections"], // User selects multiple files
   });
   console.log(filePaths);
-  if (!canceled) return filePaths[0];
+  if (!canceled) return filePaths[0]; // If dialog wasn't canceled then return selected path
 }
 
+// Create window object
 const createWindow = () => {
   const { screen } = require("electron");
 
   const primaryDisplay = screen.getPrimaryDisplay();
-  const { width, height } = primaryDisplay.workAreaSize;
+  const { width, height } = primaryDisplay.workAreaSize; // Make window size of screen
 
   const win = new BrowserWindow({
     width: width,
     height: height,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
+      preload: path.join(__dirname, "preload.js"), // Load preload.js, giving renderer and main processes access to specific functions
     },
   });
 
@@ -35,18 +38,21 @@ const createWindow = () => {
 };
 
 app.whenReady().then(() => {
-  createWindow();
+  createWindow(); // Instantiate window
 
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 
+  // Listen for submit form from renderer
   ipcMain.on("submit-form", function (event, data) {
     console.log(data);
   });
+  // Listen to openFile from renderer, call handleFileOpen, send result back to renderer
   ipcMain.handle("dialog:openFile", handleFileOpen);
 });
 
+// Quit app on close unless on Mac
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
 });
--- renderer.js
+++ renderer.js
@@ -2,9 +2,10 @@ let form = document.querySelector("form");
 let openFileButton = document.getElementById("openFile");
 let filePathElement = document.getElementById("filePath");
 
+// Add onclick to open file button
 openFileButton.addEventListener("click", async () => {
-  const filePath = await window.electronAPI.openFile();
-  filePathElement.value = filePath;
+  const filePath = await window.electronAPI.openFile(); // Call openFile in main process
+  filePathElement.value = filePath; // Set value of input to selected file path
 });
 
 form.addEventListener("submit", (e) => {
